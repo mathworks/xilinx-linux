analogdevicesinc/linux

dma: pl330: Send flush command at the beginning of a transfer
Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

drivers/dma/pl330.c
@@ -1280,6 +1280,7 @@ static inline int _ldst_devtomem(unsigned dry_run, u8 buf[],
 {
 	int off = 0;
 
+	off += _emit_FLUSHP(dry_run, &buf[off], pxs->r->peri);
 	while (cyc--) {
 		off += _emit_WFP(dry_run, &buf[off], SINGLE, pxs->r->peri);
 		off += _emit_LDP(dry_run, &buf[off], SINGLE, pxs->r->peri);
@@ -1295,6 +1296,7 @@ static inline int _ldst_memtodev(unsigned dry_run, u8 buf[],
 {
 	int off = 0;
 
+	off += _emit_FLUSHP(dry_run, &buf[off], pxs->r->peri);
 	while (cyc--) {
 		off += _emit_WFP(dry_run, &buf[off], SINGLE, pxs->r->peri);
 		off += _emit_LD(dry_run, &buf[off], ALWAYS);

mathworks/xilinx-linux

drivers/dma/pl330.c
@@ -1106,6 +1106,7 @@ static inline int _ldst_devtomem(struct pl330_dmac *pl330, unsigned dry_run,
 	else
 		cond = SINGLE;
 
+	off += _emit_FLUSHP(dry_run, &buf[off], pxs->desc->peri);
 	while (cyc--) {
 		off += _emit_WFP(dry_run, &buf[off], cond, pxs->desc->peri);
 		off += _emit_LDP(dry_run, &buf[off], cond, pxs->desc->peri);
@@ -1131,6 +1132,7 @@ static inline int _ldst_memtodev(struct pl330_dmac *pl330,
 	else
 		cond = SINGLE;
 
+	off += _emit_FLUSHP(dry_run, &buf[off], pxs->desc->peri);
 	while (cyc--) {
 		off += _emit_WFP(dry_run, &buf[off], cond, pxs->desc->peri);
 		off += _emit_LD(dry_run, &buf[off], ALWAYS);