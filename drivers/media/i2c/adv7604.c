analogdevicesinc/linux

axi-hdmi-rx,adv7604: The interrupt was moved from the axi-hdmi-rx driver to the adv7604 driver.

drivers/media/i2c/adv7604.c
@@ -37,6 +37,7 @@
 #include <linux/videodev2.h>
 #include <linux/workqueue.h>
 #include <linux/of_graph.h>
+#include <linux/interrupt.h>
 
 #include <media/adv7604.h>
 #include <media/v4l2-ctrls.h>
@@ -2744,6 +2745,15 @@ static int adv7604_parse_dt(struct adv7604_state *state)
 	return 0;
 }
 
+static irqreturn_t adv7604_irq_handler(int irq, void *devid)
+{
+	struct adv7604_state *state = devid;
+
+	adv7604_isr(&state->sd, 0, NULL);
+
+	return IRQ_HANDLED;
+}
+
 static int adv7604_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
@@ -2793,6 +2803,17 @@ static int adv7604_probe(struct i2c_client *client,
 	} else {
 		v4l_err(client, "No platform data!\n");
 		return -ENODEV;
+	}
+
+	/* Request IRQ if available. */
+	if (client->irq) {
+		err = request_threaded_irq(client->irq, NULL, adv7604_irq_handler,
+					   IRQF_TRIGGER_HIGH | IRQF_ONESHOT,
+					   KBUILD_MODNAME, state);
+		if (err < 0) {
+			v4l2_err(client, "Request interrupt error\n");
+			return err;
+		}
 	}
 
 	/* Request GPIOs. */
@@ -2961,6 +2982,9 @@ static int adv7604_remove(struct i2c_client *client)
 	media_entity_cleanup(&sd->entity);
 	adv7604_unregister_clients(to_state(sd));
 	v4l2_ctrl_handler_free(sd->ctrl_handler);
+	if (client->irq)
+		free_irq(client->irq, state);
+
 	return 0;
 }
 

mathworks/xilinx-linux

drivers/media/i2c/adv7604.c
@@ -38,6 +38,7 @@
 #include <linux/v4l2-dv-timings.h>
 #include <linux/videodev2.h>
 #include <linux/workqueue.h>
+#include <linux/interrupt.h>
 #include <linux/regmap.h>
 
 #include <media/i2c/adv7604.h>
@@ -3215,6 +3216,15 @@ static int adv76xx_parse_dt(struct adv76xx_state *state)
 	return 0;
 }
 
+static irqreturn_t adv76xx_irq_handler(int irq, void *devid)
+{
+	struct adv76xx_state *state = devid;
+
+	adv76xx_isr(&state->sd, 0, NULL);
+
+	return IRQ_HANDLED;
+}
+
 static const struct regmap_config adv76xx_regmap_cnf[] = {
 	{
 		.name			= "io",
@@ -3600,12 +3610,26 @@ static int adv76xx_probe(struct i2c_client *client,
 	v4l2_info(sd, "%s found @ 0x%x (%s)\n", client->name,
 			client->addr << 1, client->adapter->name);
 
+	/* Request IRQ if available. */
+	if (client->irq) {
+		err = request_threaded_irq(client->irq, NULL, adv76xx_irq_handler,
+					   IRQF_TRIGGER_LOW | IRQF_ONESHOT,
+					   KBUILD_MODNAME, state);
+		if (err < 0) {
+			v4l2_err(client, "Request interrupt error\n");
+			goto err_entity;
+		}
+	}
+
 	err = v4l2_async_register_subdev(sd);
 	if (err)
-		goto err_entity;
+		goto err_free_irq;
 
 	return 0;
 
+err_free_irq:
+	if (client->irq)
+		free_irq(client->irq, state);
 err_entity:
 	media_entity_cleanup(&sd->entity);
 err_work_queues:
@@ -3636,6 +3660,9 @@ static int adv76xx_remove(struct i2c_client *client)
 	media_entity_cleanup(&sd->entity);
 	adv76xx_unregister_clients(to_state(sd));
 	v4l2_ctrl_handler_free(sd->ctrl_handler);
+	if (client->irq)
+		free_irq(client->irq, state);
+
 	return 0;
 }
 